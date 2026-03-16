6d0fdd15c17d405119b2359516c1862dabfdf435
Andrey <bondarevav@bcs.ru>
2026-03-16 21:36:14 +0300

    Добавил проверку на изменение несуществующего питомца

--- src/test/java/tests/testPet.java
+++ src/test/java/tests/testPet.java
@@ -1,9 +1,12 @@
+package tests;
+
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import models.Pet;
 import org.junit.jupiter.api.Test;
 
 import static io.qameta.allure.Allure.step;
@@ -36,6 +39,38 @@ public class testPet {
 
         step("Проверка текста ответа", () ->
                 assertEquals("Pet deleted", responseBody,
+                        "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody)
+        );
+    }
+
+    @Test
+    @Feature("Pet")
+    @Severity(SeverityLevel.NORMAL)
+    @Owner("Андрей Бондарев")
+    public void testUpdateNonexistentPet() {
+        Pet pet = new Pet();
+        pet.setId(9999);
+        pet.setName("Non-existent Pet");
+        pet.setStatus("available");
+
+        Response response = step("Формирование запроса на изменение несуществующего Pet", () ->
+                given()
+                        .contentType((ContentType.JSON))
+                        .header("Accept", "application/json")
+                        .body(pet)
+                        .when()
+                        .put(BASE_URL + "/pet")
+        );
+
+        String responseBody = response.getBody().asString();
+
+        step("Проверка статус-кода ответа", () ->
+                assertEquals(404, response.getStatusCode(),
+                        "Код ответа не совпал с ожидаемым. Ответ: " + responseBody)
+        );
+
+        step("Проверка текста ответа", () ->
+                assertEquals("Pet not found", responseBody,
                         "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody)
         );
     }
